e196f95141af701cd7d586f980d3fc9a1b8fc079
juancarlosfig1603 <juancarlos_fig@hotmail.com>
2021-07-23 19:44:02 -0500

    trabajo terminado

--- sales-report-app/src/components/SideBar/SideBar.jsx
+++ sales-report-app/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import './SideBar.css'
+import React from "react";
+import "./SideBar.css";
+import usuario from "./usuario_master.png";
+const user = "Juan Carlos"
 
 export default function Main() {
-    return (
-        <div className="sidebar">
-            <ul className="sidebar-list">
-                <li className="row"><div id="title"><a href="#MonthlySalesReport">Ventas Mensuales</a></div></li>
-                <li className="row"><div id="title"><a href="#ProductsSalesReport">Top Productos</a></div></li>
-                <li className="row"><div id="title"><a href="#CustomerSalesReport">Top Clientes</a></div></li>
-                <li className="row"><div id="title"><a href="#DailySalesReport">Ventas Diarias</a></div></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <div className="perfil">
+        <img src={usuario} className="img-card" />
+      </div>
+      <div className="name-user">
+          {user}
+      </div>
+      <ul className="sidebar-list">
+        <li className="row">
+          <div id="title">
+            <a href="#MonthlySalesReport">Ventas Mensuales</a>
+          </div>
+        </li>
+        <li className="row">
+          <div id="title">
+            <a href="#DailySalesReport">Ventas Diarias</a>
+          </div>
+        </li>
+        <li className="row">
+          <div id="title">
+            <a href="#ProductsSalesReport">Top Productos</a>
+          </div>
+        </li>
+        <li className="row">
+          <div id="title">
+            <a href="#CustomerSalesReport">Top Clientes</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
